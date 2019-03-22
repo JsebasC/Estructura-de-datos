40d06cb80f05419e0e1805dbbf1a3fa73ef93282
JsebasC <jhoansebastian231@hotmail.com>
2019-03-22 11:48:11 -0500

    Multilista as code final

--- multilista.cpp
+++ multilista.cpp
@@ -10,8 +10,8 @@ struct lista{
 	lista *ap1;
 	lista *ap2;
 	
-}*cab,*nodo,*fin,*p1,*p2;
-
+}*cab,*nodo,*fin,*p1,*p2,*p3,*p4;
+void borrar_salon();
 void capturar(){
 	system ("cls");
 	nodo=new lista;
@@ -26,109 +26,74 @@ void capturar(){
 	if(!cab){ // el primer elemento se agrega aca
 		cab=nodo;
 		fin=nodo;
-	
-		
 	}else{
 		if(cab->salon>nodo->salon){ // se re ubica si es menor
-			nodo->ap1=cab;
-			cab=nodo;
-					
+			nodo->ap1=cab;	
+			cab=nodo;		
 		}else{
 			if(nodo->salon>fin->salon){ // se posiciona en el fin
 				fin->ap1=nodo;
-				
 				fin=nodo;
-				
 			}else{
-				
-				
+				p1=cab;
+/*Agregro*/		if(nodo->salon==cab->salon){ // el segundo viene aca si es igual ala cabecera cuando solo hay un elemento 
+/*Agregro*/			nodo->ap2=p1->ap2;
+/*Agregro*/			p1->ap2=nodo;
+/*Agregro*/			cout<<"Se agrego en el salon "<<nodo->salon<<" ya existente";
+/*Agregro*/			getch();
+				}else{ 
 					p1=cab;
-					
-					int insertar=0;
 					p2=cab->ap1;
-					
+					int insertar=0;
 					while(insertar==0){ // se posiciona en la mitad
-						if(p1==cab && p1->salon==nodo->salon){
-								system("pause");
-								nodo->ap2=p1->ap2;
-								p1->ap2=nodo;
-								cout<<"Se agrego cab en el salon "<<nodo->salon<<" ya existente";
-								insertar=1;
-								getch();
-						}else{
-							
-							if(nodo->salon<p2->salon){
-								
-								if(nodo->salon==p1->salon){
-									nodo->ap2=p1->ap2;
-									p1->ap2=nodo;
-									cout<<"Se agrego en el salon "<<nodo->salon<<" ya existente";
-									insertar=1;
-									getch();
-								}else{
-									nodo->ap1=p2;
-									p1->ap1=nodo;
-									insertar=1;
-								}
-							}else{
-								
-								p1=p2;
-								p2=p1->ap1;
-							}
-						
-							if(p1==fin && p1->salon==nodo->salon){ // el ultimo elemento repetido se agrega aca
+						if(nodo->salon<p2->salon){
+							if(nodo->salon==p1->salon){
 								nodo->ap2=p1->ap2;
 								p1->ap2=nodo;
 								cout<<"Se agrego en el salon "<<nodo->salon<<" ya existente";
 								insertar=1;
 								getch();
-							}
-						}		
+							}else{
+								nodo->ap1=p2;
+								p1->ap1=nodo;
+								insertar=1;
+							}		
+						}else{	
+							p1=p2;
+							p2=p1->ap1;
+						}	
+						if(p1==fin && p1->salon==nodo->salon){ // el ultimo elemento repetido se agrega aca
+							nodo->ap2=p1->ap2;
+							p1->ap2=nodo;
+							cout<<"Se agrego en el salon "<<nodo->salon<<" ya existente";
+							insertar=1;
+							getch();
+						}
 					}
 					
-				
+				}	
 			}
 		}
 	}
-	
 }
 
 void mostrar_salon(){
 	if(!cab){
-		cout<<"No existen elementos ";
-		
+		cout<<"No existen elementos ";	
 	}else{
 		system("cls");
 		cout<<"----------Salon----------"<<"\n";
-	
-
-		
-		int salir=0;
-			
-		nodo=cab;	
-	while(nodo!=NULL ){
+		nodo=cab;
+		while(nodo!=NULL){
 		cout<<"\t   ";	cout<<nodo->salon;cout<<"\n";
-	
-	//	cout<<"Salon : ";cout<<nodo->salon;cout<<" Codigo : ";cout<<nodo->codigo;cout<<"\n";
-		
-		nodo=nodo->ap1;
-	
-	
-
-	}
-	
-		
+			nodo=nodo->ap1;
+		}
 	}
 	cout <<"\nEmter Para Continuar \n";
-	getch();  //comando de espera mientras se presiona una tecla
-	
+	getch();  //comando de espera mientras se presiona una tecla	
 }
-
-
-
-
 void borrar(){
-		//buscamos el primer nodo de la lista Pito Cola y la retiramos 
+		//buscamos el primer nodo de la lista Pila Cola y la retiramos 
 	if(!cab)
 	{
 		cout << "Lista No Creada";
@@ -145,159 +110,276 @@ void borrar(){
 }
 
 
-
-void borrar_especial(void)
-{
-	
-	// borramos un nodo en cualquier parte de la lista
-	// se busca y se retira
-	int borrar_cod;    // se define variable local
-	int seguir_borrar; // se define variable local
-	
-	if(!cab)
-	{
-		cout << "La lista no exite";
-	}
-	else
-	{
-		cout << "Digite la unidad que desea borrar: ";
-		cin >> borrar_cod;
-	
-
-			p1 = cab; 
-			int buscar=0; 
-            seguir_borrar = 0;			
-			while ( seguir_borrar == 0 )
-			{
-				if(p1->salon == borrar_cod)
-				{
-					buscar=1; 
-					               // indicando que encontramos al menos un nodo a borrar
-					if(cab == p1 ) // cuando borremos el primer elemento
-					{
-						if (cab == fin ) // En el momento que el primer elemento a borrar sea el unico de la lista 
-		                {
-		                	cab = NULL; // como borramos este nodo y es cab = Fin = p1  la lista queda en "NULL"
-		                	delete p1;
-		                	seguir_borrar = 1; //como borramos el ultimo Nodo 
-							                   //no seguimos buscando mas nodos desactivamos la variable "seguir_borrar" 						    
-						}				
-						else
-						{ 	cab = p1->ap1; // como el elemento aborrar es el primero la variable cabecera "cab"
-						                 // se posiciona en el siguiente "nodo"
-						    delete p1;   // borramos el nodo encontrado
-						    p1 = cab;
-						    seguir_borrar = 1;
-						}
+void borrar_buscar(){
+	int borrar; int seguir;
+	cout<<"digite salon a borrar = ";
+	cin>>borrar;
+	if(!cab){
+		cout<<"La lista no existe";
+	}else{
+		p1=cab;
+		int buscar;
+		seguir=0;
+		while(p1->salon<=borrar && seguir==0){
+			if(p1->salon==borrar && borrar<=p1->salon){
+				buscar=1;
+				if(cab==p1){
+					if(cab==fin){ // cuando la cabecera esta sola
+						cab=NULL;
+						delete p1;
+						seguir=1;
+					}else{
+						cab=p1->ap1; // la cabecera pasa al segundo lugar
+						delete p1;
+						p1=cab;
 					}
-					else
-					{
-						if (fin == p1) // en el momento que borremos el ultimo registro
-						{	
-						    fin = p2;       // retrocedemos el FIN al nodo anterior "p2"
-						    p2->ap1 = NULL;  // Como llegamos al ultimo el siguiente es NULL
-							delete p1;
-						    seguir_borrar = 1; //como borramos el ultimo Nodo 
-							                   //no seguimos buscando mas nodos desactivamos la variable "seguir_borrar" 						    
-							seguir_borrar = 1;							                   
-						}
-						else 
-						{
-							p2->ap1 = p1->ap1;					
-    					    delete p1;
-	    				    p1 = p2->ap1;
-	    				    seguir_borrar = 1;
-						}					    						
+				}else{
+					if(fin==p1){ // fin se devuelve a p2
+						fin=p2; 
+						p2->ap1=NULL;
+						delete p1;
+						seguir=1;
+					}else{
+						p2->ap1=p1->ap1; // si esta en la mitad conecta con los otros
+						delete p1;
+						p1=p1->ap1;
 					}
 				}
-				else
-				{	p2 = p1;  // en este bloque avanzamos en la lista con las variable "p1" y "p2" 
-					p1 = p1->ap1;  // para seguir en la busqueda
-				}
-			}
-			
-			if(buscar == 0)
-			{
-				cout << "No existe este Numero \n";
-			}
-			else
-			{
-				cout <<"se retiro el Nodo \n";
-			}
-
+			}else{
+				p2=p1;
+				p1=p1->ap1;	
+			}	
+		}
+		if(buscar==0){
+			cout<<"No existe salon ";
+		}else{
+			cout<<"Se retiro del nodo ";
+		}	
 	}
-	cout <<"Emter Para Continuar \n";
+	cout <<"\nEnter Para Continuar \n";
 	getch();  //comando de espera mientras se presiona una tecla
 }
-
-
 void mostrar(){
 	if(!cab){
 		cout<<"No existen elementos ";
-		
 	}else{
 		system("cls");
 		cout<<"-----------------------------------\n";
 		cout<<"Salones registrados con codigo : "<<"\n";
-		cout<<"-----------------------------------"<<"\n";
-		
-		
-		nodo=cab;
-		while(nodo!=NULL ){// muestran el primer elemento
-		
+		int seguir=1;
+		p1=cab;
+		while(p1!=NULL ){
+			p2=p1;
+			cout<<"-----------------------------------\n";
+			cout<<"Salon : ";cout<<p2->salon<<"\n";
+			while(p2!=NULL){
 				
-			cout<<"Salon : ";cout<<nodo->salon;cout<<" Codigo : ";cout<<nodo->codigo;cout<<"\n";
-			nodo=nodo->ap1;	
-
-			
-		}
-		cout<<"\n";
-		cout<<"-----------------------------------";
-		cout<<"\nCodigo por cada salon\n";
-		cout<<"-----------------------------------\n";
-		
-			nodo=p1;
-		
-		while(nodo!=NULL){
-			if(nodo!=NULL){					
-					
-					if(nodo->ap2!=NULL){
-							nodo=nodo->ap2;
-							cout<<"Salon : ";cout<<nodo->salon;cout<<" Codigo : ";cout<<nodo->codigo;cout<<"\n";
-						
-					}	else{
-							nodo=nodo->ap2;
-					}
-						
-					
-				
-			}else{
-					
-				nodo=nodo->ap1;
+					cout<<" Codigo : ";cout<<p2->codigo;cout<<"\n";
+					p2=p2->ap2;
 			}
-				
-		}	
-		
+			p1=p1->ap1;
+		}
 	}
 	cout <<"\nEmter Para Continuar \n";
 	getch();  //comando de espera mientras se presiona una tecla
 }
 
 
+void borrar_salon(){ // el CODIGO PUEDE ESTAR EN CUALQUIER SALON
+	if(!cab){
+		cout<<"No existen elementos ";	
+	}else{
+		system("cls");
+		int verificar;
+		int borrar;
+		cout<<"Codigo a buscar = \n";
+		cin>>borrar;
+		int encontrado=0;
+		p1=cab;
+		p3=cab;
+		while(encontrado==0 && p1!=NULL){
+				if(p1->codigo==borrar){
+					verificar=1;
+					if(cab==p1){
+						if(cab->ap1==NULL && cab->ap2==NULL){// la cabecera es el unico elemento
+							cab=NULL;
+							delete p1;	
+							encontrado=1;
+						}else{
+							if(cab->ap2==NULL){ // si cab no tiene elmentos abajo, cabecera sera el del lado
+								cab=cab->ap1;	
+								delete p1;
+								encontrado=1;
+							}else{ //cab pasara a ser el de abajo
+							
+								p1=cab;
+								cab=cab->ap2;
+								encontrado=1;
+								cab->ap1=p1->ap1;
+							}
+						}	
+					}else{
+						if(fin==p1){
+							if(fin->ap2==NULL){ // si fin no tiene elementos abajo este se devolvera
+									fin=p4;
+									p4->ap1=NULL;
+									delete p1;
+									encontrado=1;
+							}else{ // si fin tiene elementos abajo pues este pasara hacer cabecera de	
+								fin=fin->ap2;
+								p3=fin;
+								encontrado=1;
+								p4->ap1=fin;
+								fin=p3->ap1;
+							}
+						}else{ // si encuentra en la mitad
+							if(p1->ap2==NULL && p1==p3){ // si enuentra en la mitad pero no hay nada abajo	
+								p4->ap1=p3->ap1;
+								delete p1;
+								p3=p3->ap1;
+								encontrado=1;
+							}else{
+								if(p1==p3){// si es elemento de la mitad pero hay elemtnos abajo este pasa hacer incio								
+								p1=p1->ap2;
+								p1->ap1=p3->ap1;
+								encontrado=1;
+								p4->ap1=p1;	
+								}else{ // se borra si encuentra verticalmente pero no al inicio
+									p2->ap2=p1->ap2;
+									delete p1;
+									p1=p1->ap2;
+									encontrado=1;	
+								}
+							}
+						}
+					}
+				}else{
+					// movimiento
+						if(p1->ap2==NULL){ 
+							p4=p3;
+							p3=p3->ap1;
+							p1=p3;
+						}else{
+							p2=p1;
+							p1=p1->ap2;			
+						}
+				}
+		}
+		if(verificar==0){
+			cout<<"No existe Codigo ";
+		}else{
+			cout<<"Se retiro del nodo ";
+		}
+	}	
+	cout <<"\nEmter Para Continuar \n";
+	getch();  //comando de espera mientras se presiona una tecla
+}
+
+void borrar_salon_codigo(){	//BORRAR EL CODIGO QUE ENCUENTRE EN EL SALON
+	if(!cab){
+		cout<<"No existen elementos ";
+	}else{
+		system("cls");
+		int verificar;
+		int borrar,salon;
+		cout<<"Salon a buscar = \n";
+		cin>>salon;
+		cout<<"Codigo a buscar = \n";
+		cin>>borrar;
+		int encontrado=0;
+		p1=cab;
+		p3=cab;
+		while(encontrado==0 && p1!=NULL){
+				if(p1->codigo==borrar && p1->salon==salon){ //CONDICION PARA ENCONTRAR EL CODIGO EN SU SALON
+					verificar=1;
+					if(cab==p1){
+						if(cab->ap1==NULL && cab->ap2==NULL){// la cabecera es el unico elemento
+							cab=NULL;
+							delete p1;
+							encontrado=1;
+						}else{
+							if(cab->ap2==NULL){ // si cab no tiene elmentos abajo, cabecera sera el del lado
+								cab=cab->ap1;	
+								delete p1;
+								encontrado=1;
+							}else{ //cab pasara a ser el de abajo	
+								p1=cab;
+								cab=cab->ap2;
+								encontrado=1;
+								cab->ap1=p1->ap1;
+							}
+						}
+					}else{
+						if(fin==p1){
+							if(fin->ap2==NULL){ // si fin no tiene elementos abajo este se devolvera
+									fin=p4;
+									p4->ap1=NULL;
+									delete p1;
+									encontrado=1;
+							}else{ // si fin tiene elementos abajo pues este pasara hacer cabecera de		
+								fin=fin->ap2;
+								p3=fin;
+								encontrado=1;
+								p4->ap1=fin;
+								fin=p3->ap1;
+							}
+						}else{ // si encuentra en la mitad
+							if(p1->ap2==NULL && p1==p3){ // si enuentra en la mitad pero no hay nada abajo	
+								p4->ap1=p3->ap1;
+								delete p1;
+								p3=p3->ap1;
+								encontrado=1;
+							}else{
+								if(p1==p3){// si es elemento de la mitad pero hay elemtnos abajo este pasa hacer incio								
+								p1=p1->ap2;
+								p1->ap1=p3->ap1;
+								encontrado=1;
+								p4->ap1=p1;			
+								}else{ // se borra si encuentra verticalmente pero no al inicio
+									p2->ap2=p1->ap2;
+									delete p1;
+									p1=p1->ap2;
+									encontrado=1;	
+								}
+							}
+						}
+					}
+				}else{
+						if(p1->ap2==NULL){
+							p4=p3;
+							p3=p3->ap1;
+							p1=p3;
+						}else{
+							p2=p1;
+							p1=p1->ap2;				
+						}
+				}
+	}
+		if(verificar==0){
+			cout<<"No existe Codigo ";
+		}else{
+			cout<<"Se retiro del nodo ";
+		}
+}	
+	cout <<"\nEmter Para Continuar \n";
+	getch();  //comando de espera mientras se presiona una tecla
+}
 
 void menu(){
 system ("color 0b" );
 	int op;
-	while(op!=6){
+	while(op!=8){
 		system ("cls");
 		cout<<"----------Digite----------"<<"\n";
 		
 		cout<<"1: Capturar "<<"\n";
-		cout<<"2: Mostrar salones "<<"\n";
-		cout<<"3: Mostrar salones con codigo "<<"\n";
-		cout<<"4: Borrar salon  "<<"\n";
-		cout<<"5: Buscar salon borrar "<<"\n";
-		cout<<"6: Salir "<<"\n";
+		cout<<"2: Mostrar salones registrados "<<"\n";
+		cout<<"3: Mostrar todo "<<"\n";
+		cout<<"4: Borrar salon por orden"<<"\n";
+		cout<<"5: Buscar salon a borrar "<<"\n";
+		cout<<"6: Buscar codigo a borrar "<<"\n";
+		cout<<"7: Buscar salon con codigo para borrar "<<"\n";
+		cout<<"8: Salir "<<"\n";
 		cout<<"----------Opcion----------"<<"\n";
 		cout<<"\t    ";cin>>op;
 		
@@ -306,14 +388,12 @@ system ("color 0b" );
 			case 2: mostrar_salon();break;
 			case 3: mostrar();break;
 			case 4: borrar();break;
-			case 5: borrar_especial(); break;
-			
-			
-						
+			case 5: borrar_buscar();break;
+			case 6: borrar_salon(); break;
+			case 7: borrar_salon_codigo(); break;			
 		}
 	}
 }
-
 
 int main(){
 	menu();
